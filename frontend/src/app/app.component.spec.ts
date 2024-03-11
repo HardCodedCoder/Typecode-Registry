902ca501e31620cc4acadde233694a55b7e341bd
HardCodedCoder <44021409+HardCodedCoder@users.noreply.github.com>
2024-03-11 12:50:02 +0100

    Restructured frontend directory structure

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'typecode-registry-frontend' title`, () => {
+  it(`should have the 'frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('typecode-registry-frontend');
+    expect(app.title).toEqual('frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, typecode-registry-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
   });
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'typecode-registry-frontend';
+  title = 'frontend';
 }
