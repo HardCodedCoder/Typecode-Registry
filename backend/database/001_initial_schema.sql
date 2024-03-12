70466862d75df709f50262fdcb1a2145199bcc6c
HardCodedCoder <44021409+HardCodedCoder@users.noreply.github.com>
2024-03-12 19:55:02 +0100

    feat: #6 created sql script for creating database model in PostgresSQL

---
+++ backend/database/001_initial_schema.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS role_assignment;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS extension;
+DROP TABLE IF EXISTS project;
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE "user" (
+      id SERIAL PRIMARY KEY,
+      oauth_identifier VARCHAR(255) UNIQUE NOT NULL,
+      email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE project (
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(255) NOT NULL,
+     description TEXT,
+     creation_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE extension (
+       id SERIAL PRIMARY KEY,
+       project_id INT,
+       name VARCHAR(255) NOT NULL,
+       description TEXT,
+       scope VARCHAR(50),
+       FOREIGN KEY (project_id) REFERENCES project(id)
+);
+
+
+CREATE TABLE item (
+        id INT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        extension_id INT REFERENCES extension (id),
+        table_name VARCHAR(255), -- As discussed table must not be provided by hybris types.
+        typecode INT NOT NULL
+);
+
+CREATE TABLE role_assignment (
+     role_assignment_id SERIAL PRIMARY KEY,
+     user_id INT REFERENCES "user"(id) NOT NULL,
+     project_id INT REFERENCES project(id) NOT NULL,
+     role VARCHAR(255) NOT NULL
+);
