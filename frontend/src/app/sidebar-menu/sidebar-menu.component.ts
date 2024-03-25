e891123e5d60e6c9be86b8e4cd7311ebbf7a6cad
NiklasKahrFH <2210859006@fh-burgenland.at>
2024-03-25 10:24:07 +0100

    feat: TRN-38 #1 created sidebar-menu and typecode-editor components

---
+++ frontend/src/app/sidebar-menu/sidebar-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-menu',
+  templateUrl: './sidebar-menu.component.html',
+  styleUrl: './sidebar-menu.component.scss'
+})
+export class SidebarMenuComponent {
+
+}
---
+++ frontend/src/app/typecode-editor/typecode-editor.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TypecodeEditorComponent } from './typecode-editor.component';
+
+describe('TypecodeEditorComponent', () => {
+  let component: TypecodeEditorComponent;
+  let fixture: ComponentFixture<TypecodeEditorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TypecodeEditorComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(TypecodeEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
