ed8023f9e690b74b2d848b73df5cfec7a436365f
HardCodedCoder <44021409+HardCodedCoder@users.noreply.github.com>
2024-03-29 19:19:58 +0100

    chore: TRN-38 #24 Fixed Unit tests and increased coverage back to 100%

--- frontend/src/app/sidebar-menu/sidebar-menu.component.spec.ts
+++ frontend/src/app/sidebar-menu/sidebar-menu.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarMenuComponent } from './sidebar-menu.component';
 
 describe('SidebarMenuComponent', () => {
@@ -18,5 +17,26 @@ describe('SidebarMenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have isActive initially as false', () => {
+    expect(component.isActive).toBe(false);
+  });
+
+  it('should toggle is Active when toggle method is called', () => {
+    component.toggle();
+    expect(component.isActive).toBe(true);
+    component.toggle();
+    expect(component.isActive).toBe(false);
+  });
+
+  it('should emit toggleSidebar event with correct value when toggle method is called', () => {
+    spyOn(component.toggleSidebar, 'emit');
+
+    component.toggle();
+    expect(component.toggleSidebar.emit).toHaveBeenCalledWith(true);
+
+    component.toggle();
+    expect(component.toggleSidebar.emit).toHaveBeenCalledWith(false);
   });
 });
--- frontend/src/app/sidebar-menu/sidebar-menu.component.ts
+++ frontend/src/app/sidebar-menu/sidebar-menu.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
-  styleUrl: './sidebar-menu.component.scss',
+  styleUrls: ['./sidebar-menu.component.scss'],
 })
 export class SidebarMenuComponent {
   @Output() toggleSidebar = new EventEmitter<boolean>();
