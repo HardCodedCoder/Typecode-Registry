959de7640214103308911531bdff1374235906bc
HardCodedCoder <44021409+HardCodedCoder@users.noreply.github.com>
2024-04-10 02:02:20 +0200

    chore: check if this fixes the server issue.

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -1,4 +1,3 @@
-console.log('Lade Karma-Konfiguration...');
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -34,9 +33,12 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true,
+    proxies: {
+      "/assets/": "/base/src/assets/"
+    },
   });
 };
